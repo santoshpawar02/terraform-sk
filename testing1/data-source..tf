8d3d3ee64e14348b0bd91483d36d18d97782cba8
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-26 15:26:47 +0530

    data-source

---
+++ testing1/data-source..tf
@@ -0,0 +1,12 @@
+data "aws_vpc" "data-source" {
+  id = "vpc-04417dbce3e787069"
+}
+
+
+resource "aws_internet_gateway" "data-igw" {
+  vpc_id = aws_vpc.data-source.id
+
+  tags = {
+    Name = "data-igw"
+  }
+}
