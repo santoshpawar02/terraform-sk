e258ce9c1b8be39344d1c02aaefae2b7bc29f287
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-27 20:10:53 +0530

    tags

---
+++ day2/test1/s3.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "my-tf-test-bucket12457881" {
+  bucket = "my-tf-test-bucket12457881"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my-tf-test-bucket12457882" {
+  bucket = "my-tf-test-bucket12457882"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "my-tf-test-bucket12457883" {
+  bucket = "my-tf-test-bucket12457883"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
