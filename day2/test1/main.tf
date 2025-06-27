21754ec8d1c7cbd66a65fa54c0573bc8788c6756
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-27 19:55:46 +0530

    key

--- day2/test1/main.tf
+++ day2/test1/main.tf
@@ -87,10 +87,10 @@ resource "aws_security_group" "allow_all" {
 }
 
 resource "aws_instance" "web-1" {
-  ami                         = "ami-0866a3c8686eaeeba"
-  availability_zone           = var.ec2_az
-  instance_type               = var.ec2_type
-  key_name                    = var.key_name
+  ami               = "ami-0866a3c8686eaeeba"
+  availability_zone = var.ec2_az
+  instance_type     = var.ec2_type
+  #  key_name                    = var.key_name
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
