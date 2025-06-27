f28ecf2bb016b49e1e15a232a07dd215918fc955
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-27 21:33:37 +0530

    key_name

--- day2/test1/ec2-prod.tf
+++ day2/test1/ec2-prod.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web-1" {
   ami                         = "ami-0866a3c8686eaeeba"
   availability_zone           = "us-east-1a"
   instance_type               = "t2.micro"
-  key_name                    = "SecOps-Key"
+  key_name                    = "SecOps-Ke"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   associate_public_ip_address = true
@@ -14,6 +14,6 @@ resource "aws_instance" "web-1" {
   }
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
   }
 }
