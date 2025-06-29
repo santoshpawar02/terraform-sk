34cba0cfcbed5ea8e0d22af9c9b1a089140765c2
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-29 17:28:58 +0530

    count

--- day5/test1/route.tf
+++ day5/test1/route.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table_association" "public-subnet" {
-  count          = 3
+  count          = length(var.public_cidr_block)
   subnet_id      = element(aws_subnet.public-subnet.*.id, count.index)
   route_table_id = aws_route_table.public_route_table.id
 }
 
 
 resource "aws_route_table_association" "private-subnet" {
-  count          = 3
+  count          = length(var.private_cidr_block)
   subnet_id      = element(aws_subnet.private-subnet.*.id, count.index)
   route_table_id = aws_route_table.private_route_table.id
 }
--- day5/test1/subnet.tf
+++ day5/test1/subnet.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "public-subnet" {
-  count             = 3 #starts from 0-2
+  #  count             = 3 #starts from 0-2
+  count             = length(var.public_cidr_block)
   vpc_id            = aws_vpc.default.id
   cidr_block        = element(var.public_cidr_block, count.index + 1)
   availability_zone = element(var.azs, count.index)
@@ -15,7 +16,8 @@ resource "aws_subnet" "public-subnet" {
 
 
 resource "aws_subnet" "private-subnet" {
-  count             = 3 #starts from 0-2
+  #  count             = 3 #starts from 0-2
+  count             = length(var.private_cidr_block)
   vpc_id            = aws_vpc.default.id
   cidr_block        = element(var.private_cidr_block, count.index + 1)
   availability_zone = element(var.azs, count.index)
