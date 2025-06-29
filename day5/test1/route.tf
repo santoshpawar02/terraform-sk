939c66a6ea5c6de4cad439d4df3326aed355e199
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-29 17:20:24 +0530

    -route-table

--- day5/test1/route.tf
+++ day5/test1/route.tf
@@ -1,11 +1,11 @@
-resource "aws_route_table_association" "public-subnets" {
+resource "aws_route_table_association" "public-subnet" {
   count          = 3
   subnet_id      = element(aws_subnet.public-subnet.*.id, count.index)
   route_table_id = aws_route_table.public_route_table.id
 }
 
 
-resource "aws_route_table_association" "private-subnets" {
+resource "aws_route_table_association" "private-subnet" {
   count          = 3
   subnet_id      = element(aws_subnet.private-subnet.*.id, count.index)
   route_table_id = aws_route_table.private_route_table.id
