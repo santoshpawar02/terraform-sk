0d28467e4e99125b04651a0d528d6cc9fec9f587
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-06-29 16:57:25 +0530

    fresh

--- day5/test1/variable.tf
+++ day5/test1/variable.tf
@@ -3,7 +3,6 @@ variable "vpc_cidr" {}
 variable "vpc_name" {}
 #variable "key_name" {}
 variable "azs" {}
-variable "environment" {}
 variable "public_cidr_block" {}
 variable "private_cidr_block" {}
 variable "environment" {}
