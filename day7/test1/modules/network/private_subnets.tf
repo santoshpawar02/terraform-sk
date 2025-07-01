e63070af5b16c66398df506fd21f78e9aac570c0
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-07-01 21:14:34 +0530

    + 1

--- day7/test1/modules/network/private_subnets.tf
+++ day7/test1/modules/network/private_subnets.tf
@@ -2,11 +2,11 @@ resource "aws_subnet" "private-subnet" {
   #count             = 3 #012
   count             = length(var.private_cird_block)
   vpc_id            = aws_vpc.default.id
-  cidr_block        = element(var.private_cird_block, count.index)
+  cidr_block        = element(var.private_cird_block, count.index + 1)
   availability_zone = element(var.azs, count.index)
 
   tags = {
-    Name        = "${var.vpc_name}-private-subnet-${count.index}"
+    Name        = "${var.vpc_name}-private-subnet-${count.index + 1}"
     Owner       = local.Owner
     costcenter  = local.costcenter
     TeamDL      = local.TeamDL
--- day7/test1/modules/network/public_subnets.tf
+++ day7/test1/modules/network/public_subnets.tf
@@ -2,11 +2,11 @@ resource "aws_subnet" "public-subnet" {
   #count             = 3 #012
   count             = length(var.public_cird_block)
   vpc_id            = aws_vpc.default.id
-  cidr_block        = element(var.public_cird_block, count.index)
+  cidr_block        = element(var.public_cird_block, count.index + 1)
   availability_zone = element(var.azs, count.index)
 
   tags = {
-    Name        = "${var.vpc_name}-public-subnet-${count.index}"
+    Name        = "${var.vpc_name}-public-subnet-${count.index + 1}"
     Owner       = local.Owner
     costcenter  = local.costcenter
     TeamDL      = local.TeamDL
