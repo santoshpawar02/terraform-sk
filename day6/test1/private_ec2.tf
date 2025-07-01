27c66d3aad42f35dbe4f41731bc28aa54d4d6d2a
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-07-01 12:52:24 +0530

    null

--- day6/test1/private_ec2.tf
+++ day6/test1/private_ec2.tf
@@ -14,17 +14,17 @@ resource "aws_instance" "private-server" {
     Team        = local.Team
     environment = "var.environment"
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update
-    sudo apt install nginx -y
-    sudo apt install git -y
-    sudo git clone https://github.com/saikiranpi/SecOps-game.git
-    sudo rm -rf /var/www/html/index.nginx-debian.html
-    sudo cp  SecOps-game/index.html /var/www/html/index.html
-    echo "<h1>${var.vpc_name}-private-server-${count.index + 1}</h1>" >> /var/www/html/index.html
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-EOF
+  #   user_data = <<-EOF
+  #     #!/bin/bash
+  #     sudo apt update
+  #     sudo apt install nginx -y
+  #     sudo apt install git -y
+  #     sudo git clone https://github.com/saikiranpi/SecOps-game.git
+  #     sudo rm -rf /var/www/html/index.nginx-debian.html
+  #     sudo cp  SecOps-game/index.html /var/www/html/index.html
+  #     echo "<h1>${var.vpc_name}-private-server-${count.index + 1}</h1>" >> /var/www/html/index.html
+  #     sudo systemctl start nginx
+  #     sudo systemctl enable nginx
+  # EOF
 
 }
--- day6/test1/user-data.sh
+++ day6/test1/user-data.sh
@@ -6,7 +6,7 @@ user_data = <<-EOF
     sudo git clone https://github.com/saikiranpi/SecOps-game.git
     sudo rm -rf /var/www/html/index.nginx-debian.html
     sudo cp  SecOps-game/index.html /var/www/html/index.html
-    echo "<h1>${var.vpc_name}-private-Server-${count.index + 1}</h1>" >> /var/www/html/index.html
+    echo "<h1>${var.vpc_name}-private-server-${count.index + 1}</h1>" >> /var/www/html/index.html
     sudo systemctl start nginx
     sudo systemctl enable nginx
 EOF
