682afe3c202c436222c091ee042f8f09f54fe3cf
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-07-01 21:03:41 +0530

    environment

--- day7/test1/modules/network/output.tf
+++ day7/test1/modules/network/output.tf
@@ -11,11 +11,11 @@ output "environment" {
 }
 
 output "public-subnet" {
-  value = aws_subnet.public-subnet.id
+  value = aws_subnet.public-subnet.*.id
 }
 
 output "private-subnet" {
-  value = aws_subnet.private-subnet.id
+  value = aws_subnet.private-subnet.*.id
 }
 
 
