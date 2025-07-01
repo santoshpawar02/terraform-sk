4a919db036bc70869c465c60b97be87d692aa0fc
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-07-01 13:45:31 +0530

    LaptopKey.pem

--- day6/test1/null.tf
+++ day6/test1/null.tf
@@ -10,4 +10,20 @@ resource "null_resource" "cluster" {
       host        = element(aws_instance.public-server.*.public_ip, count.index)
     }
   }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 777 /tmp/user-data.sh",
+      "sudo /tmp/user-data.sh",
+      "sudo apt update",
+      "sudo apt install jq unzip -y",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("LaptopKey.pem")
+      host        = element(aws_instance.public-server.*.public_ip, count.index)
+    }
+  }
+
 }
